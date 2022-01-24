851a763858d938848b3c6e010688ed6805268171
CamilleAbella <camille.abella@hotmail.fr>
2022-01-25 00:10:45 +0100

    init

---
+++ src/commands/fight.ts
@@ -0,0 +1,17 @@
+// view and manage stuff
+
+import { SlashCommandBuilder } from "@discordjs/builders"
+
+export default new SlashCommandBuilder()
+  .setName("fight")
+  .setDescription("Start ranked fight")
+  .addSubcommand(sub => sub
+    .setName("friendly")
+    .setDescription("Start friendly fight")
+    .addUserOption(option =>
+      option
+        .setName("friend")
+        .setDescription("Friend to fight")
+        .setRequired(true)
+     )
+  )
---
+++ src/commands/stuff.ts
@@ -0,0 +1,17 @@
+// view and manage stuff
+
+import { SlashCommandBuilder } from "@discordjs/builders"
+
+export default new SlashCommandBuilder()
+  .setName("stuff")
+  .setDescription("Show stuff")
+  .addSubcommand(sub =>
+      sub
+        .setName("sell")
+        .setDescription("Sell stuff")
+  )
+  .addSubcommand(sub =>
+    sub
+      .setName("remove")
+      .setDescription("Remove stuff")
+  )
